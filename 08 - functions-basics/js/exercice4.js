49f585dc39ed089850e7d56e0cff2e17d0cc45b5
cecdeweb <deweb.bordeaux@gmail.com>
2023-10-03 09:56:00 +0200

    add ex javascript basics ex4

--- 08 - functions-basics/js/exercice4.js
+++ 08 - functions-basics/js/exercice4.js
@@ -5,3 +5,17 @@ format "jour/mois/année, heure:minute:seconde".
 4. Écrivez une fonction `calculerAge` qui prend une année de naissance en paramètre 
 et retourne l'âge actuel de la personne.
  */
+function showDate(){
+    let dateToDay = new Date();
+    console.log(dateToDay.toLocaleString('Fr'));
+    let day = dateToDay.getDay();
+    let month = dateToDay.getMonth();
+    let year = dateToDay.getFullYear();
+    let hour = dateToDay.getHours();
+    let minute = dateToDay.getMinutes();
+    let seconde = dateToDay.getSeconds();
+    let dateFormat = day + "/" + month + "/" + "/" + year + " , " + hour + ":" + minute + ":" + seconde;
+    console.log(dateFormat);
+    return dateFormat;
+}
+showDate();
--- 08 - functions-basics/js/exercice5.js
+++ 08 - functions-basics/js/exercice5.js
@@ -1,1 +1,25 @@
-// Your code goes here
+/**## Exercice 5 : Manipulation des Fonctions
+Dans cet exercice, vous allez manipuler des fonctions en JavaScript.
+3. Écrivez une fonction `creerFonction` qui prend un nombre `n` en paramètre et 
+retourne une nouvelle fonction qui ajoute `n` à un nombre donné.
+4. Utilisez la fonction créée pour créer deux fonctions : `ajouter5` qui ajoute 5 
+à un nombre, et `ajouter10` qui ajoute 10 à un nombre.
+5. Utilisez ces fonctions pour effectuer des opérations mathématiques.
+ */
+let n = parseFloat(prompt("Choisir un nombre"));
+function run(n){
+    function addNumber(number){
+        let addN = parseFloat(prompt("Ajouter un nombre au précédent :"));
+        number+=addN;
+        console.log(number);
+        function AddFive(number){
+            let nbForAddFive = parseFloat("Ajouter un nouveau nombre pour qu'on lui ajoute 5 :");
+            nbForAddFive+=5;
+            console.log(nbForAddFive);
+            return nbForAddFive;
+        }
+        return AddFive();
+    }
+    return addNumber();
+}
+run(n);
